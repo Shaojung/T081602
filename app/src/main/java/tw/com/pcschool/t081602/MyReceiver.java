69b938abffdcea1b7696745499d501f492386435
shaojung <shaojung@gmail.com>
2016-08-16 16:19:55 +0800

    Second Commit

---
+++ app/src/main/java/tw/com/pcschool/t081602/MyReceiver.java
@@ -0,0 +1,21 @@
+package tw.com.pcschool.t081602;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.d("B", "收到廣播");
+
+        // throw new UnsupportedOperationException("Not yet implemented");
+
+    }
+}
