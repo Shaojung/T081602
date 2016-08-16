468f2a97cd404817780cbcb926d1374ce8016f9f
shaojung <shaojung@gmail.com>
2016-08-16 16:00:54 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/t081602/MyService.java
@@ -0,0 +1,46 @@
+package tw.com.pcschool.t081602;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Date;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    private Handler handler = new Handler();
+    private Runnable showTime = new Runnable() {
+        public void run() {
+// 顯示目前時間
+            Log.i("mylog", new Date().toString());
+            handler.postDelayed(this, 1000);
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("mylog", "onStartCommand()");
+        handler.post(showTime);
+        return Service.START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        Log.i("mylog", "onDestroy()");
+        handler.removeCallbacks(showTime);
+        super.onDestroy();
+    }
+}
